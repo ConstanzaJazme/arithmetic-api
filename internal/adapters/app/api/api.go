2926f5fe135763372531c182ed89d061c8283c23
ConstanzaJazme <constanzajazmeh@gmail.com>
2022-12-20 23:20:34 -0300

    feat: add framework layer with db operations

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"fmt"
 	"hex/internal/ports"
 )
 
 type Adapter struct {
+	db    ports.DbPort
 	arith ports.ArithmeticPort
 }
 
-func NewAdater(arith ports.ArithmeticPort) *Adapter {
-	return &Adapter{arith: arith}
+func NewAdater(db ports.DbPort, arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{db: db, arith: arith}
 }
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
@@ -20,35 +20,55 @@ func (apia Adapter) GetAddition(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	err = apia.db.AddToHistory(answer, "addition")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetSubtraction(a, b int32) (int32, error) {
-    answer, err := apia.arith.Subtraction(a, b)
+	answer, err := apia.arith.Subtraction(a, b)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return answer, nil
+	err = apia.db.AddToHistory(answer, "subtraction")
+	if err != nil {
+		return 0, err
+	}
+
+	return answer, nil
 }
 
 func (apia Adapter) GetMultiplication(a, b int32) (int32, error) {
-    answer, err := apia.arith.Multiplication(a, b)
+	answer, err := apia.arith.Multiplication(a, b)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return answer, nil
+	err = apia.db.AddToHistory(answer, "multiplication")
+	if err != nil {
+		return 0, err
+	}
+
+	return answer, nil
 }
 
 func (apia Adapter) GetDivision(a, b int32) (int32, error) {
-    answer, err := apia.arith.Division(a, b)
+	answer, err := apia.arith.Division(a, b)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return answer, nil
+	err = apia.db.AddToHistory(answer, "division")
+	if err != nil {
+		return 0, err
+	}
+
+	return answer, nil
 }
